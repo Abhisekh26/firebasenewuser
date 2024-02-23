f4a155f2e382948d156a2c8d589a4b8c6d1d0d3d
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-02-23 12:02:27 +0530

    security feature

--- auth-context.js
+++ auth-context.js
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const intialToken=localStorage.getItem("token")
+  const [token, setToken] = useState(intialToken);
 
   const userIsLoggedIn = !!token;
 
   function loginHandler(token) {
     setToken(token);
+    localStorage.setItem('token',token)
   }
 
   function logoutHandler() {
     setToken(null);
+    localStorage.removeItem("token")
   }
   const contextValue = {
     token: token,
